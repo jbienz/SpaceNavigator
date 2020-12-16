e1a3b644182fd04f9f7ee8d2b4bb7d28e511f2ca
Jared Bienz <jbienz@microsoft.com>
2020-12-16 14:33:13 -0600

    Rmoved PlayTrans and PlayRot setting because I don't need different values at runtime, the GUI is not set to update runtime settings, and the default runtime settings are way too slow.

--- Space Navigator Unity project/Assets/SpaceNavigator/Plugins/SpaceNavigator.cs
+++ Space Navigator Unity project/Assets/SpaceNavigator/Plugins/SpaceNavigator.cs
@@ -5,50 +5,62 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-namespace SpaceNavigatorDriver {
+namespace SpaceNavigatorDriver
+{
 
-	public abstract class SpaceNavigator : IDisposable {
-		// Public runtime API
-		public static Vector3 Translation {
-			get { return Instance.GetTranslation(); }
-		}
-		public static Quaternion Rotation {
-			get { return Instance.GetRotation(); }
-		}
-		public static Quaternion RotationInLocalCoordSys(Transform coordSys) {
-			return coordSys.rotation * Rotation * Quaternion.Inverse(coordSys.rotation);
-		}
-		public static void SetTranslationSensitivity(float newPlayTransSens) {
-			Settings.PlayTransSens = newPlayTransSens;
-		}
-		public static void SetRotationSensitivity(float newPlayRotSens) {
-			Settings.PlayRotSens = newPlayRotSens;
-		}
+    public abstract class SpaceNavigator : IDisposable
+    {
+        // Public runtime API
+        public static Vector3 Translation
+        {
+            get { return Instance.GetTranslation(); }
+        }
+        public static Quaternion Rotation
+        {
+            get { return Instance.GetRotation(); }
+        }
+        public static Quaternion RotationInLocalCoordSys(Transform coordSys)
+        {
+            return coordSys.rotation * Rotation * Quaternion.Inverse(coordSys.rotation);
+        }
+        public static void SetTranslationSensitivity(float newPlayTransSens)
+        {
+            // Settings.PlayTransSens = newPlayTransSens;
+            Settings.TransSens[Settings.CurrentGear] = newPlayTransSens;
+        }
+        public static void SetRotationSensitivity(float newPlayRotSens)
+        {
+            // Settings.PlayRotSens = newPlayRotSens;
+            Settings.RotSens = newPlayRotSens;
+        }
 
-		// Abstract members
-		public abstract Vector3 GetTranslation();
-		public abstract Quaternion GetRotation();
+        // Abstract members
+        public abstract Vector3 GetTranslation();
+        public abstract Quaternion GetRotation();
 
-		#region - Singleton -
-		public static SpaceNavigator Instance {
-			get {
-				if (_instance == null) {
-#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
-					_instance = SpaceNavigatorWindows.SubInstance;
-#endif
-#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-				_instance = SpaceNavigatorMac.SubInstance;
-#endif
-				}
-				return _instance;
-			}
-			set { _instance = value; }
-		}
-		private static SpaceNavigator _instance;
-		#endregion - Singleton -
+        #region - Singleton -
+        public static SpaceNavigator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+                    _instance = SpaceNavigatorWindows.SubInstance;
+                    #endif
+                    #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                    _instance = SpaceNavigatorMac.SubInstance;
+                    #endif
+                }
+                return _instance;
+            }
+            set { _instance = value; }
+        }
+        private static SpaceNavigator _instance;
+        #endregion - Singleton -
 
-		#region - IDisposable -
-		public abstract void Dispose();
-		#endregion - IDisposable -
-	}
+        #region - IDisposable -
+        public abstract void Dispose();
+        #endregion - IDisposable -
+    }
 }
